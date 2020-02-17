99e8064eb7c22e0421322baefca445bcd2a39181
Justin Swiger <justin.swiger@me.com>
2020-02-16 23:49:54 -0500

    added add page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom'
-import Search from './pages/Search'
+import Add from './pages/Add'
 import Favorites from './pages/Favorites'
 import HomePage from './pages/HomePage'
 import NotFound from './pages/NotFound'
@@ -9,15 +9,24 @@ const App = () => {
   return (
     <>
       <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/add">Add</Link>
+            </li>
+          </ul>
+        </nav>
         <section className="titlePage">
           <header>
             <h2>Things I Do, You Never Knew</h2>
-            {/* <h2>You Never Knew</h2> */}
           </header>
         </section>
         <Switch>
           <Route exact path="/" component={HomePage}></Route>
-          <Route exact path="/search" component={Search}></Route>
+          <Route exact path="/add" component={Add}></Route>
           <Route exact path="/favorites" component={Favorites}></Route>
           <Route path="*" component={NotFound}></Route>
         </Switch>
---
+++ src/pages/Add.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Add = () => {
+  return (
+    <>
+      <section>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Submit</button>
+      </section>
+    </>
+  )
+}
+
+export default Add
